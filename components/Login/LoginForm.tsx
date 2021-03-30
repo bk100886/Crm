f75532f87b0c59308f9f406c5f739c5d9da3e606
kbesedin <company@devgrand.com>
2021-03-30 15:40:01 +0300

    логин

--- components/Login/LoginForm.tsx
+++ components/Login/LoginForm.tsx
@@ -1,36 +1,56 @@
 import React from 'react'
 import Link from 'next/link'
 import 'semantic-ui-css/semantic.min.css'
-import { Button, Form, Grid, Message, Segment } from 'semantic-ui-react'
+import { Button, Form, Grid, Message, Segment, Icon, Container } from 'semantic-ui-react'
 
-export default class LoginForm extends React.Component{
-  constructor(props:any){
+interface IProps {
+  
+}
+
+interface IState {
+  errorMessage: string
+}
+
+export default class LoginForm extends React.Component<IProps, IState>{
+  constructor(props:IProps){
     super(props)
+    this.state={
+      errorMessage:""
+    }
   }
-
+  
   render() {
     return (
       <div>
         <Grid textAlign='center'  verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Form size='large'>
-            <Segment basic>
-              <Form.Input fluid icon='user' iconPosition='left' placeholder='Электронная почта' />
+        <Segment basic>
+          {this.state.errorMessage==="" ? null :
+          <Message error>
+            <Container textAlign="left">
+              <Icon name='exclamation' />
+              {this.state.errorMessage}
+            </Container>
+          </Message>
+          }
+          <Form>
+              <Form.Input fluid icon='user' iconPosition='left' placeholder='Электронная почта' required />
               <Form.Input
                 fluid
                 icon='lock'
                 iconPosition='left'
                 placeholder='Пароль'
                 type='password'
+                required
               />
-              <Button primary fluid size='large'>
+              <Button primary fluid>
                 Войти
               </Button>
-              <Message>
-                  Впервые в системе? <Link href="/register"><a>Зарегистироваться</a></Link>
-              </Message>
-            </Segment>
           </Form>
+          <Message>
+              Впервые в системе? <Link href="/register"><a>Зарегистироваться</a></Link>
+          </Message>
+          </Segment>
         </Grid.Column>
       </Grid>
       </div>
