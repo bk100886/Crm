2aa22c04fab94ccfd875879133ff115bf0c5a346
kbesedin <company@devgrand.com>
2021-03-30 11:53:54 +0300

    убрал лишний линк

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react'
 import { Header } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
-import Link from 'next/link'
 import Head from 'next/head'
 import UnauthorizedNavMenu from './UnauthorizedNavMenu'
 
