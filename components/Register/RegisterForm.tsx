48ce656884ae511643777b83866907374af13f07
kbesedin <company@devgrand.com>
2021-03-31 12:41:22 +0300

    ывавыа

--- components/Register/RegisterForm.tsx
+++ components/Register/RegisterForm.tsx
@@ -1,22 +1,65 @@
 import React from 'react'
 import Link from 'next/link'
 import 'semantic-ui-css/semantic.min.css'
-import { Button, Form, Grid, Message, Segment, Icon, Container } from 'semantic-ui-react'
-
+import { Button, Form, Grid, Message, Segment, Container } from 'semantic-ui-react'
+import { validateEmail } from '../../utils/EmailValidator'
 interface IProps {
   
 }
 
 interface IState {
-    errorMessage: string
+    errorMessage: string,
+    name:string,
+    email:string,
+    password1:string,
+    password2:string
 }
 
 export default class RegisterForm extends React.Component<IProps, IState>{
     constructor(props:IProps){
     super(props)
     this.state={
-        errorMessage:""
+        errorMessage:"",
+        name:"",
+        email:"",
+        password1:"",
+        password2:""
     }
+  }
+
+  onSubmit=()=>{
+    if (this.state.name=="" ||
+        this.state.email=="" ||
+        this.state.password1=="" ||
+        this.state.password2==""){
+          return;
+        }
+    if (!validateEmail(this.state.email)){
+      this.setState({errorMessage:"Некорректный формат адреса электронной почты"});
+      return;
+    }
+    if (this.state.password1!==this.state.password2){
+      this.setState({errorMessage:"Пароли не совпадают"});
+      this.setState({password1:"", password2:""});
+      return;
+    }
+    this.setState({errorMessage:"Не удалось выполнить вход с указанными учетными данными"});
+  }
+
+  onNameChange=(value:string)=>{
+    this.setState({name:value});
+  }
+
+  onEmailChange=(value:string)=>{
+    this.setState({email:value});
+  }
+
+  onPassword1Change=(value:string)=>{
+    this.setState({password1:value});
+  }
+
+  onPassword2Change=(value:string)=>{
+    this.setState({password2:value});
   }
   
   render() {
@@ -28,7 +71,6 @@ export default class RegisterForm extends React.Component<IProps, IState>{
           {this.state.errorMessage==="" ? null :
           <Message error>
             <Container textAlign="left">
-              <Icon name='exclamation' />
               {this.state.errorMessage}
             </Container>
           </Message>
@@ -38,13 +80,15 @@ export default class RegisterForm extends React.Component<IProps, IState>{
               fluid icon='user' 
               iconPosition='left' 
               placeholder='Ваше имя' 
-              required />
+              required 
+              onChange={event=>this.onNameChange(event.target.value)}/>
 
             <Form.Input 
               fluid icon='mail' 
               iconPosition='left' 
               placeholder='Электронная почта' 
-              required />
+              required 
+              onChange={event=>this.onEmailChange(event.target.value)}/>
 
             <Form.Input
               fluid
@@ -53,7 +97,8 @@ export default class RegisterForm extends React.Component<IProps, IState>{
               placeholder='Пароль'
               type='password'
               required
-            />
+              value={this.state.password1}
+              onChange={event=>this.onPassword1Change(event.target.value)}/>
 
             <Form.Input
               fluid
@@ -62,9 +107,13 @@ export default class RegisterForm extends React.Component<IProps, IState>{
               placeholder='Подтверждение пароля'
               type='password'
               required
-            />
+              value={this.state.password2}
+              onChange={event=>this.onPassword2Change(event.target.value)}/>
 
-            <Button primary fluid>
+            <Button 
+              primary 
+              fluid
+              onClick={()=>this.onSubmit()}>
               Зарегистироваться
             </Button>
           </Form>
